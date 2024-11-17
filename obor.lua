26ff6ce8fb1d51c1378205084d50497e5e600390
Milo:3 Silli Denote <71358745+MiloX3-Silli-Denote@users.noreply.github.com>
2024-11-16 21:30:11 -0500

    Add files via upload

---
+++ obor.lua
@@ -0,0 +1,126 @@
+--! do not remove this, credit is asked to be given
+--[[
+
+https://github.com/MiloX3-Silli-Denote/lua-obor
+
+MIT License
+
+Copyright (c) 2024 Milo:3 Silli Denote
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+]]
+
+local Obor = {};
+
+Obor.__index = Obor;
+
+--? check if object is a userdata or a table
+function isData(obj)
+    local tp = type(obj);
+
+    return tp == "userdata" or tp == "table";
+end
+
+--? determine if a is (sub)object of b
+function is(a, b)
+    if not isData(a) or not isData(b) then
+        return false; -- comparing to a non object
+    end
+
+    local meta_b = b.__super and b or getmetatable(b); -- check if b is a super, if not then get its super
+
+    if not meta_b or not meta_b.__super then
+        return false; -- b is not a an obor
+    end
+
+    local meta_a = a.__super and a or getmetatable(a);
+
+    while meta_a do
+        if meta_a == meta_b then
+            return true; -- a was a subobject of b
+        end
+
+        meta_a = getmetatable(meta_a);
+    end
+
+    return false; -- a was not a subobject of b
+end
+
+
+--! NOT TO BE OVERWRITTEN
+function Obor:__call(...) -- metamethod
+    if not self.__super then
+        self = getmetatable(self);
+
+        assert(self.__super, "tried to create instance of non object");
+    end
+
+    local instance = setmetatable({__super = false}, self);
+    instance:new(...);
+
+    return instance;
+end
+
+-- default :new() function to prevent crashing when not redefined
+function Obor:new() -- meant to be overwritten
+    return self;
+end
+
+--? create a new object that returns true when is() is called comparing new object to current
+function Obor:extend()
+    local class = setmetatable({}, self);
+
+    class.__index = class;
+    class.__super = true;
+
+    class.__call = self.__call;
+
+    return class;
+end
+
+--? inherit functions from given object
+function Obor:inherit(obj)
+    if not obj.__super then
+        obj = getmetatable(obj);
+
+        assert(obj and obj.__super, "tried to inherit from non-object");
+    end
+
+    local tempPairs = obj.__pairs; -- prevent shenanigans
+    obj.__pairs = pairs;
+
+    for k, v in pairs(obj) do
+        if type(v) == "function" then
+            self[k] = v;
+        end
+    end
+
+    self.__pairs = tempPairs;
+    obj.__pairs = tempPairs; -- allow shenanigans
+
+    return self;
+end
+
+--? quick function for extending and inheriting
+function Obor:pull()
+    return self:extend():inherit(self);
+end
+
+return Obor;
---
+++ test.lua
@@ -0,0 +1,82 @@
+local Obor = require("obor");
+
+local Math = Obor:extend(); -- class container
+
+local Vec = Math:extend();     -- object
+local Mat = Math:extend();     -- object
+local Triangle = Math:extend(); -- object
+
+function Vec:new(x, y)
+    self[1] = x or 0;
+    self[2] = y or x or 0;
+end
+
+function Vec:__index(key)
+    if key == "x" then
+        return self[1];
+    elseif key == "y" then
+        return self[2];
+    end
+
+    if Vec[key] then
+        return Vec[key];
+    end
+
+    error("couldnt index Vec with \"" .. key .. "\"");
+end
+
+function Vec:__add(a)
+    assert(is(a, Vec), "tried to add non vec");
+
+    return Vec(self.x + a.x, self.y + a.y);
+end
+
+local Vec3 = Vec:pull();
+
+function Vec3:new(x, y, z)
+    self[1] = x or 0;
+    self[2] = y or x or 0;
+    self[3] = z or y or x or 0;
+end
+
+function Mat:new(a, b, c, d)
+    self.a = a or 0;
+    self.b = b or 0;
+    self.c = c or 0;
+    self.d = d or 0;
+end
+
+function Mat:__mul(a)
+    assert(is(a, Math), "tried to mul mat by non vec/mat");
+
+    if is(a, Vec) then
+        return Vec(self.a * a.x + self.b * a.y, self.c * a.x + self.d * a.y);
+    elseif is(a, Mat) then
+        return Mat(self.a * a.a + self.b * a.c, self.a * a.b + self.b * a.d, self.c * a.a + self.d * a.c, self.c * a.b + self.d * a.d);
+    end
+
+    error("couldnt mul mat by math object");
+end
+
+local vec_a = Vec(5, 5);
+local vec_b = Vec(10, 5);
+
+local vec_c = vec_a + vec_b; -- should be 15, 10
+print(vec_c.x, vec_c.y);
+
+local mat_a = Mat(2, 0, 0, 2); -- scale vec by 2
+
+local vec_d = mat_a * vec_c; -- should be 30, 20
+print(vec_d.x, vec_d.y);
+
+local mat_b = Mat(0, 1, 1, 0);
+local mat_c = mat_a * mat_b; -- should be : 0, 2, 2, 0
+print(mat_c.a, mat_c.b, mat_c.c, mat_c.d);
+
+local vec_3 = Vec3(2, 2, 2);
+local vec_e = vec_3 + vec_3; -- should be 4, 4
+print(vec_e.x, vec_e.y);
+
+
+--local err_1 = vec_d + mat_a; -- tried to add non vec
+--local err_2 = mat_a * Triangle(); -- couldnt mul mat by math object
